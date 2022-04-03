c487e87cab1bca11e7935f0678e6ef861cbd12d7
999Newone999 <boris_mail@list.ru>
2022-04-03 13:24:32 +0300

    Добавьте файлы проекта.

---
+++ Gusev_ITMO_Lab23/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gusev_ITMO_Lab23
+{
+    class Program
+    {
+        /*Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.*/
+
+        static double MethodFactorial(int n)
+        {
+            double factorial = 1;
+            if (n == 0)
+            {
+                factorial = 1;
+            }
+            else
+            {
+                for (int i = 1; i < n + 1; i++)
+                {
+                    factorial = factorial * i;
+                }
+            }
+            return factorial;
+        }
+        static async Task<double> FactorialAsync(int n)
+        {
+            double result = await Task.Run(() => MethodFactorial(n));
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            bool inputOk = true;
+            int n = 1;
+            do
+            {
+                inputOk = true;
+                Console.Write("Число, факториал которого надо найти: ");
+                try
+                {
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    inputOk = false;
+                }
+            } while (!inputOk);
+            double factorial = FactorialAsync(n).Result;
+            Console.Write("{0}! = {1}", n, factorial);
+            Console.ReadKey();
+        }
+    }
+}
